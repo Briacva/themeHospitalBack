9cb2fff5bdaabc4b01a208fe7c10724239e479b9
abiba12 <ndeyediop.tech@gmail.com>
2022-04-11 11:45:07 +0200

    fisrt commit

---
+++ theme_hospital/src/test/java/com/simplon/theme_hospital/ThemeHospitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplon.theme_hospital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThemeHospitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
