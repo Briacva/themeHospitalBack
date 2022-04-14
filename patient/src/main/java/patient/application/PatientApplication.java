32aa31f61880c538550ce21e98283990df954544
Briacva <96231132+Briacva@users.noreply.github.com>
2022-04-14 17:02:10 +0200

    initialisation

--- patient/src/main/java/patient/application/PatientApplication.java
+++ patient/src/main/java/patient/application/PatientApplication.java
@@ -1,10 +1,33 @@
 package patient.application;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+import net.minidev.json.JSONObject;
+@RestController
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class PatientApplication {
+	
+	@Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    private RestTemplate restTemplate;
+	
+	@RequestMapping("/")
+    public String home() {
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("message", "hello Patient");
+        return jsonObject.toString();
+    }
 
 	public static void main(String[] args) {
 		 SpringApplication.run(PatientApplication.class, args);
